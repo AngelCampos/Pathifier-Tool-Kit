1b5a1bba31283af377d6c95abcde35b911ab1cee
Angel García-Campos <xxwildfangxx@gmail.com>
2015-07-15 18:45:33 -0500

    Coding style changes
    Coding Style changes

--- Heatmap.for.Pathifier.R
+++ Heatmap.for.Pathifier.R
@@ -34,7 +34,7 @@ pathways <- read.delim("pathways.txt", header = F)
 rownames(pathways) <- pathways[,2] # Naming rownames
 
 # Extract NUMBER of pathways that succesfully used Pathifier
-pathwaysInPDS <- rownames(PDSmatrix)  
+pathwaysInPDS <- rownames(PDSmatrix)
 
 # Selecting the NAMES of pathways
 paths <- as.vector(pathways[,1])      # Extracting pathway names in vector
@@ -48,20 +48,20 @@ rownames(PDSmatrix) <- labels         # assign labels as row names in PDSmatrix
 ###############################################################################
 
 # creates a own color palette passing from blue, green yellow to dark red
-my_palette <- colorRampPalette(c("blue", "cyan", "chartreuse1", "yellow", 
+my_palette <- colorRampPalette(c("blue", "cyan", "chartreuse1", "yellow",
                                  "red", "firebrick4"))(n = 1000)
 
 # (optional) defines the color breaks manually for a "skewed" color transition
-# col_breaks = c(seq(-1,0,length=100),               
-#                seq(0,0.8,length=100),              
-#                seq(0.8,1,length=100))              
+# col_breaks = c(seq(-1,0,length=100),
+#                seq(0,0.8,length=100),
+#                seq(0.8,1,length=100))
 
 ###############################################################################
 ## Clustering Methods
 ###############################################################################
 
-# If you want to change the default clustering method (complete linkage method 
-# with Euclidean distance measure), this can be done as follows: For non-square 
+# If you want to change the default clustering method (complete linkage method
+# with Euclidean distance measure), this can be done as follows: For non-square
 # matrix, we can define the distance and cluster based on our matrix data by
 
 row.distance = dist(PDSmatrix, method = "euclidean")
@@ -70,13 +70,13 @@ row.cluster = hclust(row.distance, method = "complete")
 col.distance = dist(t(PDSmatrix), method = "euclidean")
 col.cluster = hclust(col.distance, method = "complete")
 
-# Arguments for the dist() function are: euclidean (default), maximum, canberra, 
+# Arguments for the dist() function are: euclidean (default), maximum, canberra,
 # binary, minkowski, manhattan
 
-# And arguments for hclust(): complete (default), single, average, mcquitty, 
-# median, centroid, ward. 
+# And arguments for hclust(): complete (default), single, average, mcquitty,
+# median, centroid, ward.
 
-# NOTE that for non-square matrices you have to define the distance and cluster 
+# NOTE that for non-square matrices you have to define the distance and cluster
 # for both row and column dendrograms separately.
 # Otherwise you will get a not so pleasant Error in:
 # x[rowInd, colInd] : subscript out of bounds.
@@ -95,7 +95,7 @@ col.cluster = hclust(col.distance, method = "complete")
 ## Plotting the Heatmap!! (where all colorful things happen...)
 ###############################################################################
 
-png("heatmap.png", # Name of png file       
+png("heatmap.png", # Name of png file
     width = 6 * 500,      # Easier scaling 6*500 = 3000 pixels
     height = 6 * 400,     # 6 x 400 = 2400 px
     units = "px",         # px (Pixels = default), in (inches), cm or mm
@@ -107,19 +107,19 @@ heatmap.2(PDSmatrix,
           density.info = "none",  # turns off density plot inside color legend
           trace = "none",         # turns off trace lines inside the heat map
           margins = c(10,21),     # widens margins around plot
-          col = my_palette,        # use on color palette defined earlier 
+          col = my_palette,        # use on color palette defined earlier
           Rowv = as.dendrogram(row.cluster), # apply selected clustering method
           Colv = as.dendrogram(col.cluster), # apply selected clustering method
           keysize = 0.8,           # size of color key
 #Additional Options
 ## Color labeling columns (Opt. RowSideColors for rows)
-#        ColSideColors = c(           # Grouping col-samples into two different
-#           rep("dodgerblue", 10),     # categories, e.g.Samples 1-10: blue (normals)
-#           rep("firebrick1", 90)),    # Samples 11-100 (tumors)
+#        ColSideColors = c(        # Grouping col-samples into two different
+#           rep("dodgerblue", 10), # categories e.g.Samples 1-10: blue (normals)
+#           rep("firebrick1", 90)),# Samples 11-100 (tumors)
 #           breaks= col_breaks,  # enable color transition at specified limits
 #           dendrogram= "col",   # only draw a column dendrogram (opt. "row")
 
-## Legend for ColumnSide color labeling 
+## Legend for ColumnSide color labeling
 # par(lend = 1)           # square line ends for the color legend
 # legend("topright",      # location of the legend on the heatmap plot
 #        legend = c("Normals", "Tumors"), # category labels
