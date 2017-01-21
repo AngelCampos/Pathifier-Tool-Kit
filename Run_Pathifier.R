a99262a74bb36e8a8f509160e2366c31a06b7eea
Miguel Angel García Campos <angel.campos@ciencias.unam.mx>
2017-01-20 19:07:58 -0600

    Important update of method!

--- Run_Pathifier.R
+++ Run_Pathifier.R
@@ -3,26 +3,25 @@
 # Author: Miguel Angel Garcia-Campos - Github: https://github.com/AngelCampos
 ################################################################################
 
-# Install/load package
+# Install/load package ##############
 source("http://bioconductor.org/biocLite.R")
-if (!require("pathifier")) {
-  biocLite("pathifier", ask =FALSE)
-  library(pathifier)
-}
+if (!require("pathifier")){biocLite("pathifier", ask=FALSE); library(pathifier)}
 
 # Load expression data for PATHIFIER
 exp.matrix <- read.delim(file =file.choose(), as.is = T, row.names = 1)
 
-# Loading Genesets annotation 
+# Load Genesets annotation 
 # NOTE: MUST contain SAME number of elements in all rows!!
-gene_sets <- as.matrix(read.delim(file = file.choose(),
-                       header = F, sep = "\t", as.is = T))
+gene_sets <- as.matrix(read.delim(file = file.choose(), header = F, sep = "\t",
+                                  as.is = T))
+
 
 #  Generate a list that contains genes in genesets
 gs <- list()
 for (i in 1:nrow(gene_sets)){
   a <- as.vector(gene_sets[i,3:ncol(gene_sets)])
-  a <- head(unique(na.omit(a)), -1)
+  a <- na.omit(a)
+  a <- a[a != ""]
   a <- matrix(a, ncol = 1)
   gs[[length(gs)+1]] <- a
   rm(a,i)
@@ -32,30 +31,54 @@ for (i in 1:nrow(gene_sets)){
 pathwaynames <- as.list(gene_sets[,1])
 
 # Generate a list that contains the previos two lists: genesets and their names
-PATHWAYS <- list()
-PATHWAYS$gs <- gs
-PATHWAYS$pathwaynames <- pathwaynames
+PATHWAYS <- list(); PATHWAYS$gs <- gs; PATHWAYS$pathwaynames <- pathwaynames
 
+# Prepare Data #################################################################
 # Extract information from binary phenotypes. 1 = Normal, 0 = Tumor
 normals <- as.vector(as.logical(exp.matrix[1,]))
-data <- as.matrix(exp.matrix[-1, ])
-dimnames(data) <- NULL
+exp.matrix <- as.matrix(exp.matrix[-1, ])
+
+# Calculate MIN_STD
+N.exp.matrix <- exp.matrix[,as.logical(normals)]
+rsd <- apply(N.exp.matrix, 1, sd)
+min_std <- quantile(rsd, 0.25)
+
+# Set min_exp & Filter low value genes
+# At least 10% of samples with values over min_exp
+min_exp <- quantile(as.vector(exp.matrix), 0.1) # Percentile 10 of data
+over <- apply(exp.matrix, 1, function(x) x > min_exp)
+G.over <- apply(over, 2, mean)
+G.over <- names(G.over)[G.over > 0.1]
+exp.matrix <- exp.matrix[G.over,]
+exp.matrix[exp.matrix < min_exp] <- min_exp
+
+# Set maximum 5000 genes with more variance
+V <- names(sort(apply(exp.matrix, 1, var), decreasing = T))[1:5000]
+V <- V[!is.na(V)]
+exp.matrix <- exp.matrix[V,]
+genes <- rownames(exp.matrix) # Checking genes
+# dimnames(exp.matrix) <- NULL
 allgenes <- as.vector(rownames(exp.matrix))
 
 # Generate a list that contains previous data: gene expression, normal status,
 # and name of genes
-DATASET <- list(); DATASET$allgenes <- allgenes[-1]
-DATASET$normals <- normals
-DATASET$data <- data
+DATASET <- list(); DATASET$allgenes <- allgenes; DATASET$normals <- normals
+DATASET$data <- exp.matrix
 
 # Run Pathifier
-PDS<-quantify_pathways_deregulation(DATASET$data, DATASET$allgenes,
+PDS<-quantify_pathways_deregulation(DATASET$data, 
+                                    DATASET$allgenes,
                                     PATHWAYS$gs,
                                     PATHWAYS$pathwaynames,
-                                    DATASET$normals, attempts = 100,
-                                    logfile="logfile.txt", min_std = 0.1889568)
+                                    DATASET$normals, 
+                                    maximize_stability = T,
+                                    attempts = 100,
+                                    logfile="logfile.txt",
+                                    min_std = min_std,
+                                    min_exp = min_exp)
 # Remove unnecesary data
-rm(gene_sets, data, exp.matrix, allgenes, DATASET, PATHWAYS)
+rm(gene_sets, exp.matrix, allgenes, DATASET, PATHWAYS, rsd, V, over, G.over, 
+   N.exp.matrix, gs, genes, min_exp, min_std, pathwaynames)
 
 # Save image into working directory
 save.image("PDS.RData")
